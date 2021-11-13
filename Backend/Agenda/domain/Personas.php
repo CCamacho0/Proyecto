0b438d57d4209f8b56a8ba23210202b9b9ca2617
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-12 20:06:12 -0600

    Update Personas.php

--- Backend/Agenda/domain/Personas.php
+++ Backend/Agenda/domain/Personas.php
@@ -1,8 +1,126 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("baseDomain.php");
 
+class Personas extends BaseDomain implements \JsonSerializable {
+
+    //atributos
+    private $PK_cedula;
+    private $nombre;
+    private $apellido1;
+    private $apellido2;
+    private $fecNacimiento;
+    private $sexo;
+    private $observaciones;
+
+    //constructores
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public static function createNullTelefonos() {
+        $instance = new self();
+        return $instance;
+    }
+
+    public static function createTelefonos($PK_cedula, $nombre, $apellido1, $apellido2, $fecNacimiento, $sexo, $observaciones, $lastUser, $lastModification) {
+        $instance = new self();
+        $instance->PK_cedula = $PK_cedula;
+        $instance->nombre = $nombre;
+        $instance->apellido1 = $apellido1;
+        $instance->apellido2 = $apellido2;
+        $instance->fecNacimiento = $fecNacimiento;
+        $instance->sexo = $sexo;
+        $instance->observaciones = $observaciones;
+        $instance->setLastUser($lastUser);
+        $instance->setLastModification($lastModification);
+        return $instance;
+    }
+
+    //propieades
+    public function getPK_cedula() {
+        return $this->PK_cedula;
+    }
+
+    public function setPK_cedula($PK_cedula) {
+        $this->PK_cedula = $PK_cedula;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getApellido1() {
+        return $this->apellido1;
+    }
+
+    public function setApellido1($apellido1) {
+        $this->apellido1 = $apellido1;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getApellido2() {
+        return $this->apellido2;
+    }
+
+    public function setApellido2($apellido2) {
+        $this->apellido2 = $apellido2;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getFecNacimiento() {
+        return $this->fecNacimiento;
+    }
+
+    public function setFecNacimiento($fecNacimiento) {
+        $this->fecNacimiento = $fecNacimiento;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getObservaciones() {
+        return $this->observaciones;
+    }
+
+    public function setObservaciones($observaciones) {
+        $this->observaciones = $observaciones;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getlastUser() {
+        return $this->lastUser;
+    }
+
+    public function setlastUser($lastUser) {
+        $this->lastUser = $lastUser;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Convertir el obj a JSON
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
+}
