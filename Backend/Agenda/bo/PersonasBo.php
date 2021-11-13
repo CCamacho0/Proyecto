8e2e926ca8ef14a14e0fb03ede6cbc2c5dfc42e7
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-12 20:05:56 -0600

    Update PersonasBo.php

--- Backend/Agenda/bo/PersonasBo.php
+++ Backend/Agenda/bo/PersonasBo.php
@@ -1,8 +1,85 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("../domain/Personas.php");
+require_once("../dao/PersonasDao.php");
+
+class PersonasBo {
+
+    private $personasDao;
+
+    public function __construct() {
+        $this->personasDao = new PersonasDao();
+    }
+
+    public function getPersonasDao() {
+        return $this->personasDao;
+    }
+
+    public function setPersonasDao(PersonasDao $personasDao) {
+        $this->personasDao = $personasDao;
+    }
+
+    //agrega a una persona a la base de datos
+    //----------------------------------------------------------------------------------
+
+    public function add(Personas $personas) {
+        try {
+            if (!$this->personasDao->exist($personas)) {
+                $this->personasDao->add($personas);
+            } else {
+                throw new Exception("La persona ya existe en la base de datos!!");
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    //modifica a una persona a la base de datos
+    //----------------------------------------------------------------------------------
+
+    public function update(Personas $personas) {
+        try {
+            $this->personasDao->update($personas);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+    
+    //eliminar a una persona a la base de datos
+    //----------------------------------------------------------------------------------
+
+    public function delete(Personas $personas) {
+        try {
+            $this->personasDao->delete($personas);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    //consulta a una persona a la base de datos
+    //----------------------------------------------------------------------------------
+
+    public function searchById(Personas $personas) {
+        try {
+            return $this->personasDao->searchById($personas);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    //consultar todas las personas de la base de datos
+    //----------------------------------------------------------------------------------
+
+    public function getAll() {
+        try {
+            return $this->personasDao->getAll();
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+}
+
+//end of the class PersonasBo
+?>
 
