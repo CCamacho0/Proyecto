7c331907c65a9135a87c22435bf48e267a4fda39
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-12 20:06:08 -0600

    Update BaseDomain.php

--- Backend/Agenda/domain/BaseDomain.php
+++ Backend/Agenda/domain/BaseDomain.php
@@ -1,8 +1,44 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class BaseDomain {
+
+    //atriburos
+    private $lastUser;
+    private $lastModification;
+
+    //construtor
+    public function __construct() {
+        
+    }
+
+    //propiedades
+    public function getLastUser() {
+        return $this->lastUser;
+    }
+
+    public function setLastUser($lastUser) {
+        $this->lastUser = $lastUser;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getLastModification() {
+        return $this->lastModification;
+    }
+
+    public function setLastModification($lastModification) {
+        $this->lastModification = $lastModification;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Convertir el obj a JSON
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
+}
+
+//end of the class
+?>
 
