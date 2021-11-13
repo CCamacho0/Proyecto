5e59e49ff60f0d5a50284ed6831d9d422d0b596f
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-12 20:06:15 -0600

    Update Telefonos.php

--- Backend/Agenda/domain/Telefonos.php
+++ Backend/Agenda/domain/Telefonos.php
@@ -1,8 +1,78 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("baseDomain.php");
 
+class Personas extends BaseDomain implements \JsonSerializable {
+
+    //atributos
+    private $PK_telefono;
+    private $FK_cedula;
+    private $descripcion;
+
+    //constructores
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public static function createNullTelefonos() {
+        $instance = new self();
+        return $instance;
+    }
+
+    public static function createTelefonos($PK_telefono, $FK_cedula, $descripcion,$lastUser, $lastModification) {
+        $instance = new self();
+        $instance->PK_telefono = $PK_telefono;
+        $instance->FK_cedula = $FK_cedula;
+        $instance->descripcion = $descripcion;
+        $instance->setLastUser($lastUser);
+        $instance->setLastModification($lastModification);
+        return $instance;
+    }
+
+    //propieades
+    public function getPK_telefono() {
+        return $this->PK_telefono;
+    }
+
+    public function setPK_telefono($PK_telefono) {
+        $this->PK_telefono = $PK_telefono;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getFK_cedula() {
+        return $this->FK_cedula;
+    }
+
+    public function setFK_cedula($FK_cedula) {
+        $this->FK_cedula = $FK_cedula;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getdescripcion() {
+        return $this->descripcion;
+    }
+
+    public function setdescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getlastUser() {
+        return $this->lastUser;
+    }
+
+    public function setlastUser($lastUser) {
+        $this->lastUser = $lastUser;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Convertir el obj a JSON
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
+}
