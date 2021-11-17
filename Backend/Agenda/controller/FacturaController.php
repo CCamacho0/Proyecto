c4d948085ec9ac6eba45877570509acbe32c809f
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-16 21:45:31 -0600

    Create FacturaController.php

---
+++ Backend/Agenda/controller/FacturaController.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once("../bo/FacturaBo.php");
+require_once("../domain/Factura.php");
+
+// Factura Controller 
+//----------------------------------------------------------------------------------
+
+if (filter_input(INPUT_POST, 'action') != null) {
+    $action = filter_input(INPUT_POST, 'action');
+
+    try {
+        $myFacturaBo = new FacturaBo();
+        $myFactura = Factura::createNullFactura();
+
+        //choose the action
+        //----------------------------------------------------------------------------------
+
+        if ($action === "add_Factura" or $action === "update_Factura") {
+            //se valida que los parametros hayan sido enviados por post
+            if ((filter_input(INPUT_POST, 'idFactura') != null) && (filter_input(INPUT_POST, 'Detalle') != null) 
+                    && (filter_input(INPUT_POST, 'asiento') != null) && (filter_input(INPUT_POST, 'FK_cedula') != null) 
+                    && (filter_input(INPUT_POST, 'FK_idgesionVuelo') != null)) {
+                
+                $myFactura->setidFactura(filter_input(INPUT_POST, 'idFactura'));
+                $myFactura->setDetalle(filter_input(INPUT_POST, 'Detalle'));
+                $myFactura->setAsiento(filter_input(INPUT_POST, 'asiento'));
+                $myFactura->setFK_cedula(filter_input(INPUT_POST, 'FK_cedula'));
+                $myFactura->setFK_idgestionVuelo(filter_input(INPUT_POST, 'FK_idgesionVuelo'));
+                
+                if ($action == "add_Factura") {
+                    $myFacturaBo->add($myFactura);
+                    echo('M~Registro Incluido Correctamente');
+                }
+                if ($action == "update_Factura") {
+                    $myFacturaBo->update($myFactura);
+                    echo('M~Registro Modificado Correctamente');
+                }
+            } else {
+                echo('E-Los valorres no fueron enviados');
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+
+        if ($action === "showAll_Factura") {//accion de consultar todos los registros
+            $resultDB = $myFacturaBo->getAll();
+            $json = json_encode($resultDB->GetArray());
+            $resultado = '{"data": ' . $json . '}';
+            if ($resultDB->RecordCount() === 0) {
+                $resultado = '{"data": []}';
+            }
+            echo $resultado;
+        }
+
+        //----------------------------------------------------------------------------------
+
+        if ($action === "show_Factura") {//accion de mostrar Facturas por ID
+            //se valida que los parametros hayan sido enviados por post
+            if (filter_input(INPUT_POST, 'idFactura') != null) {
+                $myFactura->setidFactura(filter_input(INPUT_POST, 'idFactura'));
+                $myFactura = $myFacturaBo->searchById($myFactura);
+                if ($myFactura != null) {
+                    echo json_encode(($myFactura));
+                } else {
+                    echo('E~NO Existe un cliente con el ID especificado');
+                }
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+
+        if ($action === "delete_Factura") {//accion de eliminar Facura por ID
+            //se valida que los parametros hayan sido enviados por post
+            if (filter_input(INPUT_POST, 'idFactura') != null) {
+                $myFactura->setidFactura(filter_input(INPUT_POST, 'idFactura'));
+                $myFacturaBo->delete($myFactura);
+                echo('M~Registro Fue Eliminado Correctamente');
+            }
+        }
+
+        //se captura cualquier error generado
+        //----------------------------------------------------------------------------------
+    } catch (Exception $e) { //exception generated in the business object..
+        echo("E~" . $e->getMessage());
+    }
+} else {
+    echo('M~Parametros no enviados desde el formulario'); //se codifica un mensaje para enviar
+}
+?>
+
