476670ee9ab0eae608fdd280ebdacb18658b276a
Carlos Manuel Calvo <manu.1610@hotmail.com>
2021-11-19 11:24:59 -0600

    Creacion de dominio gestion_rutas y gestion_tipoavion

--- Backend/Agenda/domain/gestion_rutas.php
+++ Backend/Agenda/domain/gestion_rutas.php
@@ -1,3 +1,95 @@
 <?php
 
+require_once("baseDomain.php");
 
+class gestion_rutas extends BaseDomain implements \JsonSerializable {
+
+    //atributos
+    private $idgestion_rutas;
+    private $dia_semana_hora;
+    private $ruta;
+    private $duracion;
+    
+    
+    
+    //constructores
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public static function createNullgestion_rutas() {
+        $instance = new self();
+        return $instance;
+    }
+
+    public static function creategestion_rutas($idgestion_rutas, $dia_semana_hora,$ruta,
+    $duracion,$lastUser, $lastModification) {
+    
+        $instance = new self();
+        $instance->idgestion_rutas = $idgestion_rutas;
+        $instance->dia_semana_hora = $dia_semana_hora;
+        $instance->ruta = $ruta;
+        $instance->duracion = $duracion;
+        $instance->setLastUser($lastUser);
+        $instance->setLastModification($lastModification);
+        return $instance;
+    }
+
+    //propiedades
+    public function getidgestion_rutas() {
+        return $this->idgestion_rutas;
+    }
+
+    public function setidgestion_rutas($idgestion_rutas) {
+        $this->idgestion_rutas = $idgestion_rutas;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getdia_semana_hora() {
+        return $this->dia_semana_hora;
+    }
+
+    public function setdia_semana_hora($dia_semana_hora) {
+        $this->dia_semana_hora = $dia_semana_hora;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getruta() {
+        return $this->ruta;
+    }
+
+    public function setruta($ruta) {
+        $this->ruta = $ruta;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getduracion() {
+        return $this->duracion;
+    }
+
+    public function setduracion($duracion) {
+        $this->duracion = $duracion;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    
+    public function getlastUser() {
+        return $this->lastUser;
+    }
+
+    public function setlastUser($lastUser) {
+        $this->lastUser = $lastUser;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Convertir el obj a JSON
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
+}
--- Backend/Agenda/domain/gestion_tipoavion.php
+++ Backend/Agenda/domain/gestion_tipoavion.php
@@ -1,2 +1,128 @@
 <?php
 
+require_once("baseDomain.php");
+
+class gestion_tipoavion extends BaseDomain implements \JsonSerializable {
+
+    //atributos
+    private $idgestion_tipoavion;
+    private $año;
+    private $modelo;
+    private $marca;
+    private $cantidad_pasajeros;
+    private $cantidad_filas;
+    private $cantidadasientos_fila;
+    
+    
+    //constructores
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public static function createNullgestion_tipoavion() {
+        $instance = new self();
+        return $instance;
+    }
+
+    public static function creategestion_tipoavion($idgestion_tipoavion, $año,$modelo,
+    $cantidad_pasajeros,$cantidad_filas,$cantidadasientos_fila,$lastUser, $lastModification) {
+    
+        $instance = new self();
+        $instance->idgestion_tipoavion = $idgestion_tipoavion;
+        $instance->año = $año;
+        $instance->modelo = $modelo;
+        $instance->cantidad_pasajeros = $cantidad_pasajeros;
+        $instance->cantidad_filas = $cantidad_filas;
+        $instance->cantidadasientos_fila = $cantidadasientos_fila;
+        $instance->setLastUser($lastUser);
+        $instance->setLastModification($lastModification);
+        return $instance;
+    }
+
+    //propiedades
+    public function getidgestion_tipoavion() {
+        return $this->idgestion_tipoavion;
+    }
+
+    public function setidgestion_tipoavion($idgestion_tipoavion) {
+        $this->idgestion_tipoavion = $idgestion_tipoavion;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getaño() {
+        return $this->año;
+    }
+
+    public function setaño($año) {
+        $this->año = $año;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getmodelo() {
+        return $this->modelo;
+    }
+
+    public function setmodelo($modelo) {
+        $this->modelo = $modelo;
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public function getmarca() {
+        return $this->marca;
+    }
+
+    public function setmarca($marca) {
+        $this->marca = $marca;
+    }
+    
+    //----------------------------------------------------------------------------------
+
+    public function getcantidad_pasajeros() {
+        return $this->cantidad_pasajeros;
+    }
+
+    public function setcantidad_pasajeros($cantidad_pasajeros) {
+        $this->cantidad_pasajeros = $cantidad_pasajeros;
+    }
+
+    //----------------------------------------------------------------------------------
+    
+    public function getcantidad_filas() {
+        return $this->cantidad_filas;
+    }
+
+    public function setcantidad_filas($cantidad_filas) {
+        $this->cantidad_filas = $cantidad_filas;
+    }
+
+    //----------------------------------------------------------------------------------
+    
+    public function getcantidadasientos_fila() {
+        return $this->cantidadasientos_fila;
+    }
+
+    public function setcantidadasientos_fila($cantidadasientos_fila) {
+        $this->cantidadasientos_fila = $cantidadasientos_fila;
+    }
+
+    //----------------------------------------------------------------------------------
+    
+    public function getlastUser() {
+        return $this->lastUser;
+    }
+
+    public function setlastUser($lastUser) {
+        $this->lastUser = $lastUser;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Convertir el obj a JSON
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
+}//fin de la clase gestion_tipoavion
