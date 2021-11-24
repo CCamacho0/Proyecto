a01546c144a45d384314377ed5fe5446781f67d5
CCamacho0 <90570485+CCamacho0@users.noreply.github.com>
2021-11-24 12:31:12 -0600

    Cambios

--- Frontend/JScript/Funciones.js
+++ Frontend/JScript/Funciones.js
@@ -1,6 +1,23 @@
 
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(function () {
+    $("#fecha").datepicker(
+            {
+                dateFormat: "yy/mm/dd"
+            }
+    );
+});
 
-
+$(document).ready(function(e){
+    $("#TipoAviones").on("click", function(){
+        $('#content').load('../TipoAviones.html');
+    });
+    
+    $("#Rutas").on("click", function(){
+        $('#content').load('../Rutas.html');
+    });
+    $("#GestionHorarios").on("click", function(){
+        $('#content').load('../GestionHorarios.html');
+    });
+    
+});
+    
