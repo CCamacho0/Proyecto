d3723410c936f9a500843511c66f1b529ffa38c6
Carlos Manuel Calvo <manu.1610@hotmail.com>
2021-12-10 15:59:05 -0600

    Check In

--- Frontend/JScript/CheckInfunciones.js
+++ Frontend/JScript/CheckInfunciones.js
@@ -2,35 +2,19 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.sold)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
+const movieSelect = document.getElementById("ticket_type");
 
 populateUI();
 
 let ticketPrice = +movieSelect.value;
 
-// Save selected movie index and price
+// Save indice del tipo de tiquete y el precio 
 function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem("selectedMovieIndex", movieIndex);
     localStorage.setItem("selectedMoviePrice", moviePrice);
 }
 
-// Update total and count
-function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll(".row .seat.selected");
-
-    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
-
-    const selectedSeatsCount = selectedSeats.length;
-
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
-
-    setMovieData(movieSelect.selectedIndex, movieSelect.value);
-}
-
-
+    
 // Get data from localstorage and populate UI
 function populateUI() {
     const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
@@ -54,7 +38,6 @@ function populateUI() {
 movieSelect.addEventListener("change", (e) => {
     ticketPrice = +e.target.value;
     setMovieData(e.target.selectedIndex, e.target.value);
-    updateSelectedCount();
 });
 
 // Seat click event
@@ -64,11 +47,9 @@ container.addEventListener("click", (e) => {
             !e.target.classList.contains("sold")
             ) {
         e.target.classList.toggle("selected");
-
-        updateSelectedCount();
+        
     }
 });
 
-// Initial count and total set
-updateSelectedCount();
+
 
