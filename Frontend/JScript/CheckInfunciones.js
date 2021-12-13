a3e23aba2d69c77cdf410e7868a96da342a78df2
Carlos Manuel Calvo <manu.1610@hotmail.com>
2021-12-13 07:22:17 -0600

    Update CheckInfunciones.js

--- Frontend/JScript/CheckInfunciones.js
+++ Frontend/JScript/CheckInfunciones.js
@@ -2,16 +2,16 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.sold)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("ticket_type");
+const seatSelect = document.getElementById("ticket_type");
 
 populateUI();
 
-let ticketPrice = +movieSelect.value;
+let ticketPrice = +seatSelect.value;
 
 // Save indice del tipo de tiquete y el precio 
-function setMovieData(movieIndex, moviePrice) {
-    localStorage.setItem("selectedMovieIndex", movieIndex);
-    localStorage.setItem("selectedMoviePrice", moviePrice);
+function setSeatData(seatIndex, seatPrice) {
+    localStorage.setItem("selectedSeatIndex", seatIndex);
+    localStorage.setItem("selectedSeatPrice", seatPrice);
 }
 
     
@@ -27,17 +27,17 @@ function populateUI() {
         });
     }
 
-    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+    const selectedSeatIndex = localStorage.getItem("selectedMovieIndex");
 
-    if (selectedMovieIndex !== null) {
-        movieSelect.selectedIndex = selectedMovieIndex;
+    if (selectedSeatIndex !== null) {
+        seatSelect.selectedIndex = selectedSeatIndex;
     }
 }
 
 // Movie select event
-movieSelect.addEventListener("change", (e) => {
+seatSelect.addEventListener("change", (e) => {
     ticketPrice = +e.target.value;
-    setMovieData(e.target.selectedIndex, e.target.value);
+    setSeatData(e.target.selectedIndex, e.target.value);
 });
 
 // Seat click event
@@ -50,6 +50,21 @@ container.addEventListener("click", (e) => {
         
     }
 });
+
+
+function recibeAsientos(){
+    
+    var asientos = new Array(90);
+    
+    for (var i = 0; i < asientos.length; i++) {
+        
+        document.getElementsByClassName("seat");
+        asientos = asientos[i];
+        
+    }    
+    
+    
+}
 
 
 
